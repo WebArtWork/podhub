169ccf260e1959d4b9b2ee5c0c3691d71553bc04
lizvinskyi <andriilizvinskyi@gmail.com>
2024-12-03 17:33:38 +0200

    add fixes

--- js/index.js
+++ js/index.js
@@ -1,1 +1,4 @@
-console.log('Hello World');
+document.querySelector(".burger-wrap").addEventListener("click", () => {
+    document.querySelector(".header__navigation").classList.toggle("header__navigation--open");
+    document.querySelector(".burger").classList.toggle("burger--close");
+});
